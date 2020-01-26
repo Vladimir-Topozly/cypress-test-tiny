93a9813e1225478e885d90c3781fd7f5b831629c
EARNIX\VladimirT <vladimirt@earnix.com>
2020-01-26 23:29:23 +0200

    'assigment'

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,7 +1,120 @@
 describe('page', () => {
+  const node = '.TreeNode__name'
+  const nnn = 'div.ReactVirtualized__Grid__innerScrollContainer > div:nth-child(10)'
+  const dropdown = '.DropdownTree__arrow-zone';
+  const clear = 'button.Button.Button--link.Tree__clear';
+  const cancel = 'button.Button.Button--link-secondary.Tree__cancel';
+  const search = '#root > div > div > div > div > div > div';
+  const arrowOne = 'li.Tree__tree-container > div > div > div > div > div:nth-child(1) > div > div > div > div'
+  const arrowTwo = 'li.Tree__tree-container > div > div > div > div > div:nth-child(2) > div > div > div > div > svg'
+
+  function openDropdown(){
+    cy.get(dropdown).click()
+  }
+
   beforeEach(() => {
     cy.visit('http://storybook.b360-dev.autodesk.com/current/iframe.html?id=dropdowntree--default');
   })
 
-  it('works', () => {})
+  it('1. The component shows a tree that contains nodes. Some of them have children.', () => {
+    openDropdown()
+    cy.get(arrowOne).click()
+    cy.get(node).contains('Node 2').should('be.visible') // I didn't really came up with how to chech this is indeed a tree with Nodes, 
+    //on the ather hand I actually validating the entire tree in scenario #3
+  })
+
+
+  it('2. You can only select one node.', () => {
+    openDropdown()
+    cy.get(arrowOne).click()
+    cy.get(node).contains('Node 2').click()
+    // Assert that dropdown is unavailable to select aother node 
+    cy.get(node).contains('Node 1').should('not.be.visible')
+    })
+
+
+it('3. You can select all nodes in the tree, even ones with children.', () =>{
+  openDropdown()
+  cy.get(arrowOne).click()
+  cy.get(arrowTwo).click()
+  // Select every Node in a loop:
+  for (let index = 9; index >= 1; index--) {
+    cy.get(search).click()
+    cy.get(node).contains('Node ' + index + '').trigger('dummy-event').click()
+    // Assert relevant Node path is selected:
+    if(index === 2){
+      cy.get(search).should('contain', 'Node 1 > ' + 'Node ' + index + '')
+    } else if(index === 1) {
+      cy.get(search).should('contain', 'Node ' + index + '')
+    }else{
+    cy.get(search).should('contain', 'Node 1 > Node 2 > ' + 'Node ' + index + '')
+    }
+  }
+})
+
+it('4.Expand/Collapse functionality is available when clicking on the arrow of the node which has children. ', () =>{
+  openDropdown()
+  // Expand the first dropdown:
+  cy.get(arrowOne).click()
+  // Assert that 'Node 2' is visible after we expand the fiest dropdown:
+  cy.get(node).contains('Node 2').should('be.visible')
+  // Expand the second dropdown
+  cy.get(arrowTwo).click()
+  // Assert that 'Node 3 - 9' are be visible after we expanded the second dropdown:
+  for (let index = 3; index < 10; index++){
+    cy.get(node).contains('Node ' + index + '').trigger('dummy-event').should('be.visible')
+  }
+  // Collapse the second dropdown:
+  cy.get(node).contains('Node 3').trigger('dummy-event')
+  cy.get(arrowTwo).click()
+  // Assert that 'Node 3 - 9' are NOT visible after we collapsed the second dropdown:
+  for (let index = 3; index < 10; index++) {
+    if(index === 4)
+    index++       // *** BUG: 'Node 4' is always visible!
+    cy.get(node).contains('Node ' + index + '').should('not.be.visible')
+  }
+  // Assert that 'Node 2' remains visible after we collapsed the second dropdown: 
+  cy.get(node).contains('Node 2').should('be.visible')
+  // Collapse the first dropdown:
+  cy.get(arrowOne).click()
+  //  Assert that 'Node 2' is not visible anymore whereas 'Node 1' still persists:
+  cy.get(node).contains('Node 2').should('not.be.visible')
+  cy.get(node).contains('Node 1').should('be.visible')
+})
+
+
+  it('5. When clicking on the label of a node with children, it will select it and not expand/collapse.', () =>{
+    openDropdown()
+    cy.get(arrowOne).click()
+    cy.get(node).contains('Node 2').click()
+    // Assert that node with given name is selected 
+    cy.get(search).should('contain', 'Node 1 > Node 2')
+    openDropdown()
+    // Assert that dropdown is not collapsed or expanded by checking visibility of certain nodes:
+    cy.get(node).contains('Node 2').should('be.visible')
+    cy.get(node).contains('Node 3').should('not.be.visible')
+  })
+
+
+  it('6. Cancel should close the dropdown and perform no action.', () => {
+    openDropdown()
+    // make sure dropdpwn is open now
+    cy.get(node).should('be.visible')
+    cy.get(cancel).click()
+    // Assert that dropdown is closed now
+    cy.get(node).should('not.be.visible')
+    cy.get(cancel).should('not.be.visible')
+  })
+
+  it('7. Clear button should clear the selection.', () => {
+    // open dropdown
+    cy.get(dropdown).click()
+    // select the fist node
+    cy.get(node).contains('Node 1').click()
+    cy.get(dropdown).click()
+    // Assert that 'clear' button is visible and click on it
+    cy.get(clear).click()
+    // Assert that 'cancel' button clears the selection
+    cy.get(search).should('contain', '')
+  })
 })
